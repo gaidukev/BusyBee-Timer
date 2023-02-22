ff8690a54ed37028377f0a2c25bbdc3a06956e6a
gaidukev <gaidukev@ualberta.ca>
2023-02-21 21:21:10 -0700

    began implementing hexagon drawing logic

--- app/src/main/java/com/gaidukev/busybeetimer/Hexagon.java
+++ app/src/main/java/com/gaidukev/busybeetimer/Hexagon.java
@@ -1,25 +1,62 @@
 package com.gaidukev.busybeetimer;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class Hexagon extends View {
-    public Hexagon(Context context) {
-        super(context);
-    }
+
+    private Paint hexagonPaint;
+    private int hexagonColor;
+    private Path hexagonPath;
 
     public Hexagon(Context context, @Nullable AttributeSet attrs) {
+
         super(context, attrs);
+        hexagonPaint = new Paint();
+
+        // get attributes specified in attrs.xml
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.Hexagon, 0, 0);
+
+        try {
+            hexagonColor = a.getInteger(R.styleable.Hexagon_hexagonColor, 0);
+        } finally {
+            a.recycle();
+        }
+
+        hexagonPaint.setColor(hexagonColor);
+        hexagonPaint.setStrokeWidth(1f);
+        hexagonPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        setWillNotDraw(false);
     }
 
-    public Hexagon(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    @Override
+    protected void onDraw(Canvas canvas){
+
+
+        int viewLeft = this.getLeft();
+        int viewRight = this.getRight();
+        int viewTop = this.getTop();
+        int viewBottom = this.getBottom();
+
+        int viewWidthHalf = ( viewRight - viewLeft) / 2;
+        int viewHeightHalf = (viewBottom - viewTop) / 2;
+
+        int radius = 0;
+        if (viewWidthHalf>viewHeightHalf)
+            radius=viewHeightHalf-30;
+        else
+            radius=viewWidthHalf-30;
+
+        hexagonPath.reset();
+        hexagonPath.moveTo((float) radius, (float) viewWidthHalf, (float) viewHeightHalf);
+
     }
 
-    public Hexagon(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-    }
 }
